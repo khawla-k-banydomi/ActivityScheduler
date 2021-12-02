2d10d01348f40f47c27f2cf077f3cf597512efd3
Khawla <91540484+khawla-k-banydomi@users.noreply.github.com>
2021-12-03 01:43:19 +0200

    Create EventController.js

---
+++ src/Controllers/EventController.js
@@ -0,0 +1,41 @@
+const createEvent {
+    try {
+        const eventObj 
+        const event
+        
+    }
+}
+const modifyEvent {
+    try {
+        const id 
+        const updatedObj 
+        const event
+       
+    }
+}
+const deleteEvent  {
+   try{
+        const id 
+        }       
+        
+}
+
+const getAllEvents {
+    try {
+        const events 
+    }
+}
+const getOneEvent {
+    try {
+        const id 
+        const event 
+        
+}
+
+const resetEvent {
+    try {
+        const id 
+        const updatedObj 
+        const event 
+    }
+}
