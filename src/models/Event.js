6c9a1a10295e354094addf0884c9bd3335942836
Khawla <91540484+khawla-k-banydomi@users.noreply.github.com>
2021-12-02 17:17:58 +0200

    Create Event.js

---
+++ src/models/Event.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var eventSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    startTime: {
+        type: Date,
+        default: Date.now()
+    },
+    endTime: {
+        type: Date,
+        default: Date.now()
+    },
+    reminderTime: {
+        type: Date,
+        default: Date.now()
+    },
+    type: {
+        type: String,
+        enum: ['urgent', 'not urgent', 'normal']
+    },
+});
+
+module.exports = mongoose.model('Event', eventSchema);
