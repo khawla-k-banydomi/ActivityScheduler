78ac2b1955464d8a9a07d212e643c62d9120ac93
Khawla <91540484+khawla-k-banydomi@users.noreply.github.com>
2021-12-09 21:50:44 +0200

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
+const { getAllEvents, getOneEvent, createEvent, modifyEvent, deleteEvent, resetEvent } = require('../controllers/EventController');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// get all events
+router.get('/', getAllEvents);
+
+// get one event
+router.get('/:id', getOneEvent);
+
+// create a new event
+router.post('/', createEvent);
+
+// modify an event
+router.patch('/:id', modifyEvent);
+
+// delete an event
+router.delete('/:id', deleteEvent);
+
+// reset an event
+router.patch('/:id', resetEvent);
 
 module.exports = router;
